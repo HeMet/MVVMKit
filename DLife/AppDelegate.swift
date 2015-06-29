33340d4a3915fe197026392cbbc88035148c8b8d
Евгений Губин <hemet.mail@gmail.com>
2015-06-29 22:34:57 +0300

    Fix for rotation bug.

--- DLife/AppDelegate.swift
+++ DLife/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow? = UIWindow(frame: UIScreen.mainScreen().bounds)
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.mainScreen().bounds)
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let feedVM = FeedViewModel()
