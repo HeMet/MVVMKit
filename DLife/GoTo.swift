bab9e00e12cd5f8dd04c3d1428422f67f2bd9353
Евгений Губин <hemet.mail@gmail.com>
2015-07-02 21:07:45 +0300

    GoTo fix.

--- DLife/GoTo.swift
+++ DLife/GoTo.swift
@@ -13,11 +13,4 @@ struct GoTo {
     static let next = present(!ViewController.self).withTransition(Transitions.show)
     static let root = present(!FeedViewController.self).withinNavView().asRoot()
     static let entry = present(!EntryViewController.self).withTransition(Transitions.show)
-
-    static let root2 = present(!Test2ViewController.self).asRoot()
-    static let popover = present(!TestViewController.self).asPopoverOn(Test2ViewController.self) { pvc, popover in
-        pvc.preferredContentSize = CGSize(width: 200, height: 300)
-        popover.sourceView = pvc.btnPopober
-        popover.sourceRect = pvc.btnPopober.bounds
-    }
 }
