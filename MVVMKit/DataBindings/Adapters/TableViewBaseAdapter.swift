3ffee5f8f4afb09995f189aff480c8bfb4389d30
Евгений Губин <hemet.mail@gmail.com>
2015-11-02 20:20:12 +0300

    Back to unowned(unsafe).

--- MVVMKit/DataBindings/Adapters/TableViewBaseAdapter.swift
+++ MVVMKit/DataBindings/Adapters/TableViewBaseAdapter.swift
@@ -24,7 +24,7 @@ public class TableViewBaseAdapter: NSObject, UITableViewDataSource, UITableViewD
     let slHeightForRowAtIndexPath = Selector("tableView:heightForRowAtIndexPath:")
     
     // Workaround: anowned(safe) cause random crashes for NSObject descendants
-    unowned let tableView: UITableView
+    unowned(unsafe) let tableView: UITableView
     public let cells: CellViewBindingManager
     public let views = ViewBindingManager()
     public let rowHeightMode: TableAdapterRowHeightModes
